763907c06b1d43b572bb992ef70aecb2006d0f72
asdasd <asdasd>
2021-10-25 23:14:20 +0200

    adsd

---
+++ site0002/scripts/main.js
@@ -0,0 +1,13 @@
+
+//Byte mellan artiklarna. alla ligger i HTML och denna funktion ser till att endast den valda artikeln syns.
+function contentSwitch(y) {
+  var content = document.getElementsByClassName('dis');
+  for (var i = 0; i < content.length; i++) {
+    if (content[i].classList.contains(y)){
+    content[i].style.display = "block";
+    }
+    else {
+      content[i].style.display = "none";
+    }
+  }
+}
